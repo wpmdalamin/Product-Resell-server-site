8bc6fa382210a29b697a7157981e70aaa755d018
wpmdalamin <mdalamin7g@gmail.com>
2022-11-30 17:46:40 +0600

    product api create

--- index.js
+++ index.js
@@ -14,7 +14,26 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hdi07kd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        productCollection = client.db('resaleproducts').collection('products');
+        categoryCollection = client.db('resaleproducts').collection('categorys');
+        userCollection = client.db('resaleproducts').collection('User');
+        bookNowCollection = client.db('resaleproducts').collection('booknow');
 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+        })
+        
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
